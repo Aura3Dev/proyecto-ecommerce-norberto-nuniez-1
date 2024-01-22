76912a17ce6e2bd64f81635c68fdd2fbe3149888
Norberto Nuñez <norbertonuniez@gmail.com>
2024-01-21 22:23:52 -0300

    corregi algunos comentarios que me quedaron

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -3,8 +3,8 @@ import React from 'react';
 const CartWidget = () => {
     return (
         <div className="cart-widget">
-        <i className="fas fa-shopping-cart"></i> {/* Utiliza el ícono que desees */}
-        <span className="notification">1</span> {/* Reemplaza con el número real del carrito */}
+        <i className="fas fa-shopping-cart"></i>
+        <span className="notification">1</span> 
         </div>
     );
 };
